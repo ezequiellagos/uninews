b8e63d0de3026ff62403e45ff3f89c0ebc0f6668
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2018-12-12 22:20:46 -0300

    update archivos

--- uninews/settings.py
+++ uninews/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4wuekdibk*_leap&e*3n8i1jk%_qyp-22f)42mum(rrp%$5lc#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uninews.datoslab.cl']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media files
 MEDIA_URL = '/media/'
