649f029dd22300b78d58cf36302a31626fd61f3c
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2018-12-17 23:55:07 -0300

    Refactor: se evita la redundancia de codigo

--- news/views.py
+++ news/views.py
@@ -9,57 +9,23 @@ def home(request):
     news = Noticia.objects.order_by('-fecha')
     
     # Paginación
-    news_per_page = 9
-    paginator = Paginator(news, news_per_page)
-    page = request.GET.get('page')
-    try:
-        news = paginator.get_page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        news = paginator.get_page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        news = paginator.get_page(paginator.num_pages)
+    news = pagination(request, news)
 
-    # Destacados
-    hoy = datetime.today()
-    semana_atras = date.today() - timedelta(days=14)
-    formato = "%Y-%m-%d" # AAAA-MM-DD
-    fecha_actual = hoy.strftime(formato)
-    fecha_pasada = semana_atras.strftime(formato)
-    news_most_view = Noticia.objects.filter(fecha__range=[fecha_pasada, fecha_actual]).order_by('-contador_visitas')[:2]
+    # Más Vistos
+    date = mostViewed()
+    news_most_view = Noticia.objects.filter(fecha__range=[date['last_date'], date['current_date']]).order_by('-contador_visitas')[:2]
 
     return render(request, "news/home.html", {'news':news, 'news_most_view':news_most_view})
-
-def detail(request, id_noticia):
-    new = get_object_or_404(Noticia, pk=id_noticia)
-    new.contador_visitas += 1
-    new.save(update_fields=['contador_visitas'])
-    return redirect(new.link_noticia)
 
 def category(request, category):
     news = Noticia.objects.order_by('-fecha').filter(categoria=category)
 
     # Noticias por Categoria
-    hoy = datetime.today()
-    semana_atras = date.today() - timedelta(days=14)
-    formato = "%Y-%m-%d" # AAAA-MM-DD
-    fecha_actual = hoy.strftime(formato)
-    fecha_pasada = semana_atras.strftime(formato)
-    news_most_view = Noticia.objects.filter(categoria=category).filter(fecha__range=[fecha_pasada, fecha_actual]).order_by('-contador_visitas')[:2]
+    date = mostViewed()
+    news_most_view = Noticia.objects.filter(categoria=category).filter(fecha__range=[date['last_date'], date['current_date']]).order_by('-contador_visitas')[:2]
 
     # Paginación
-    news_per_page = 9
-    paginator = Paginator(news, news_per_page)
-    page = request.GET.get('page')
-    try:
-        news = paginator.get_page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        news = paginator.get_page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        news = paginator.get_page(paginator.num_pages)
+    news = pagination(request, news)
 
     return render(request, "news/category.html", {'news':news, 'news_most_view':news_most_view, 'category':category})
 
@@ -67,25 +33,11 @@ def region(request, region):
     news = Noticia.objects.filter(id_universidad__region=region).order_by('-fecha')
 
     # Noticias por Categoria
-    hoy = datetime.today()
-    semana_atras = date.today() - timedelta(days=14)
-    formato = "%Y-%m-%d" # AAAA-MM-DD
-    fecha_actual = hoy.strftime(formato)
-    fecha_pasada = semana_atras.strftime(formato)
-    news_most_view = Noticia.objects.filter(categoria=category).filter(fecha__range=[fecha_pasada, fecha_actual]).order_by('-contador_visitas')[:2]
+    date = mostViewed()
+    news_most_view = Noticia.objects.filter(id_universidad__region=region).filter(fecha__range=[date['last_date'], date['current_date']]).order_by('-contador_visitas')[:2]
 
     # Paginación
-    news_per_page = 9
-    paginator = Paginator(news, news_per_page)
-    page = request.GET.get('page')
-    try:
-        news = paginator.get_page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        news = paginator.get_page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        news = paginator.get_page(paginator.num_pages)
+    news = pagination(request, news)
 
     return render(request, "news/region.html", {'news':news, 'news_most_view':news_most_view, 'region':region})
 
@@ -93,27 +45,19 @@ def university(request, alias):
     news = Noticia.objects.filter(id_universidad__alias=alias).order_by('-fecha')
 
     # Noticias por Universidad
-    hoy = datetime.today()
-    semana_atras = date.today() - timedelta(days=14)
-    formato = "%Y-%m-%d" # AAAA-MM-DD
-    fecha_actual = hoy.strftime(formato)
-    fecha_pasada = semana_atras.strftime(formato)
-    news_most_view = Noticia.objects.filter(id_universidad__alias=alias).filter(fecha__range=[fecha_pasada, fecha_actual]).order_by('-contador_visitas')[:2]
+    date = mostViewed()
+    news_most_view = Noticia.objects.filter(id_universidad__alias=alias).filter(fecha__range=[date['last_date'], date['current_date']]).order_by('-contador_visitas')[:2]
 
     # Paginación
-    news_per_page = 9
-    paginator = Paginator(news, news_per_page)
-    page = request.GET.get('page')
-    try:
-        news = paginator.get_page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        news = paginator.get_page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        news = paginator.get_page(paginator.num_pages)
+    news = pagination(request, news)
 
     return render(request, "news/university.html", {'news':news, 'news_most_view':news_most_view, 'university':Universidad.objects.get(alias=alias)})
+
+def detail(request, id_noticia):
+    new = get_object_or_404(Noticia, pk=id_noticia)
+    new.contador_visitas += 1
+    new.save(update_fields=['contador_visitas'])
+    return redirect(new.link_noticia)
 
 def statistics(request):
     # Crea objeto con la tabla de noticias
@@ -153,3 +97,24 @@ def search(request):
             news = paginator.get_page(paginator.num_pages)
     return render(request, "news/search.html", {'news':news, 'info':info, 'search':request.GET['search']})
 
+def pagination(request, news):
+    news_per_page = 10
+    paginator = Paginator(news, news_per_page)
+    page = request.GET.get('page')
+    try:
+        return paginator.get_page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        return paginator.get_page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        return paginator.get_page(paginator.num_pages)
+
+def mostViewed():
+    days_news_most_viewed = 14
+    hoy = datetime.today()
+    semana_atras = date.today() - timedelta(days=days_news_most_viewed)
+    formato = "%Y-%m-%d" # AAAA-MM-DD
+    fecha_actual = hoy.strftime(formato)
+    fecha_pasada = semana_atras.strftime(formato)
+    return {'current_date':fecha_actual, 'last_date':fecha_pasada}
