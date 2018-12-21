6e626c840004b037414a8900bc65f2f886732c6a
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2018-12-21 16:02:30 -0300

    Add: Administración para correos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Email
 
 # Register your models here.
+class EmailAdmin(admin.ModelAdmin):
+    readonly_fields = ('email', 'register', 'id')
+    list_display = ('email', 'register')
+    ordering = ('-register',)
+
+admin.site.register(Email, EmailAdmin)
